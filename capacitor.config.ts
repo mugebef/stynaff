02fbdeb8529eb06414c0c614f6cebbcc4b738aa2
mugebef <datingwithbounce@gmail.com>
2026-04-23 14:21:04 +0200

    feat: Migrate to Capacitor 5
    Removes Capacitor 4 dependencies and config files, updating project structure to align with Capacitor 5. Includes necessary .gitignore updates for Android build artifacts and Capacitor configuration.

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.styni.app',
-  appName: 'Styn',
-  webDir: 'dist'
-};
-
-export default config;
